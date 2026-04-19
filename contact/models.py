511500ead42b15114ed9e7b60625465ff963df21
luiza <luizacarla2004@gmail.com>
2026-04-18 21:07:30 -0300

    herdando do User do django e adicionando um campo em contact indicando quem criou o contato

--- contact/models.py
+++ contact/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Category(models.Model):
     class Meta:
         verbose_name_plural = 'Categories'
         verbose_name = 'Category'
-        
+
     name = models.CharField(max_length=50)
 
     def __str__(self):
@@ -22,6 +23,7 @@ class Contact(models.Model):
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
 
     def  __str__(self):
         return f'{self.last_name} {self.first_name}'
