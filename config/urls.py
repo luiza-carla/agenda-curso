cfedef97c31d35dc2bc261692059bb3b49d0fe65
luiza <luizacarla2004@gmail.com>
2026-04-20 21:59:34 -0300

    criação da página de contato criação de css da página de contato

--- config/urls.py
+++ config/urls.py
@@ -21,7 +21,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('contact.urls')),
+    path('', include(('contact.urls', 'contact'), namespace='contact')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
