feed267af9c4b371e9a5e1a70ff4fa01a791dd51
luiza <luizacarla2004@gmail.com>
2026-04-21 23:57:59 -0300

    criação da partial header e head e css

--- contact/urls.py
+++ contact/urls.py
@@ -5,5 +5,5 @@ app_name = 'contact'
 
 urlpatterns = [
     path('contact/<int:contact_id>/', views.contact, name='contact'),
-    path('', views.index, name='contact'),
+    path('', views.index, name='index'),
 ]
